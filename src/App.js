60b01102ec726e01674f707f5a311de0ac81976f
jiggy2204 <jignacio2204@gmail.com>
2020-12-15 21:33:08 -0500

    created tests in jest

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-import Split from "./composition/Split";
-import Tooltip from "./composition/Tooltip";
+import Messages from "./Messages";
 
-const firstToolTip = (
-  <Tooltip color="hotpink" message="tooltip message">
-    ipsum
-  </Tooltip>
-);
-
-const secondToolTip = (
-  <Tooltip color="#126BCC" message="another tooltip message">
-    officiis
-  </Tooltip>
-);
-
-function App() {
-  return (
-    <main className="App">
-      <Split className="left" flexBasis={2}>
-        This is the content for the left `Split`. Lorem ipsum dolor sit amet
-        consectetur, adipisicing elit. Incidunt ex velit suscipit facere
-        officia?
-        <Tooltip message="one more tooltip message">Necessitatibus?</Tooltip>
-      </Split>
-      <Split className="right">
-        This is the content for the right `Split`. Inventore aliquid cupiditate
-        suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus
-        id soluta aspernatur.
-      </Split>
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>YOUR APPLICATION NAME!</h1>
+        <div>Hello!</div>
+        <Messages name="Messages" unread={0} />
+        <Messages name="Notifications" unread={10} />
+      </div>
+    );
+  }
 }
 
 export default App;
