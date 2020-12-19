012e856459fb9a814bc7eb8ab0ae9203b30a2ce1
Jen Ignacio <jignacio2204@gmail.com>
2020-12-18 22:20:12 -0500

    added component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Messages from "./Messages";
+import TheDate from "./state/Date";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <h1>YOUR APPLICATION NAME!</h1>
         <div>Hello!</div>
+        <TheDate />
         <Messages name="Messages" unread={0} />
         <Messages name="Notifications" unread={10} />
       </div>
---
+++ src/state/Date.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class TheDate extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            foo: 'foo',
+            hello: 'Hello, world!',
+            list: [1, 2, 3],
+            obj: { nested: 'object', yes: true },
+        }
+    }
+
+    render(){
+        console.log(this.state)  
+        return(
+            <div>{this.state.hello}</div>
+        )
+    }
+}
+
+export default TheDate;
