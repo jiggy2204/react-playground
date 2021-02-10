8eddc53652210df740aaf17316643ac24f5c4055
jiggy2204 <jignacio2204@gmail.com>
2021-02-10 12:36:42 -0500

    updated react playground

--- src/App.js
+++ src/App.js
@@ -1,65 +1,8 @@
-import React from "react";
-import AddItemForm from "./shopping-list/AddItemForm";
-import ShoppingList from "./shopping-list/ShoppingList";
+import React, { Component } from "react";
+import "./App.css";
 
-class App extends React.Component {
-  state = {
-    shoppingItems: [
-      /* put stub items in here for testing */
-      { name: "apples", checked: false },
-      { name: "oranges", checked: true },
-      { name: "bread", checked: false },
-    ],
-  };
-
-  handleDeleteItem = (item) => {
-    const newItems = this.state.shoppingItems.filter((itm) => itm !== item);
-    this.setState({
-      shoppingItems: newItems,
-    });
-  };
-
-  handleCheckItem = (item) => {
-    const newItems = this.state.shoppingItems.map((itm) => {
-      if (itm === item) {
-        itm.checked = !itm.checked;
-      }
-      return itm;
-    });
-    this.setState({
-      shoppingItems: newItems,
-    });
-  };
-
-  handleAddItem = (itemName) => {
-    const newItems = [
-      ...this.state.shoppingItems,
-      { name: itemName, checked: false },
-    ];
-    this.setState({
-      shoppingItems: newItems,
-    });
-  };
-
+export default class App extends Component {
   render() {
-    return (
-      <>
-        <header>
-          <h1>Shopping List</h1>
-        </header>
-        <main>
-          <section>
-            <AddItemForm onAddItem={this.handleAddItem} />
-          </section>
-          <section>
-            <ShoppingList
-              items={this.state.shoppingItems}
-              onDeleteItem={this.handleDeleteItem}
-              onCheckItem={this.handleCheckItem}
-            />
-          </section>
-        </main>
-      </>
-    );
+    return <div className="App" />;
   }
 }
