3884708512baf6d204c582761accbfe8e5d4c560
jiggy2204 <jignacio2204@gmail.com>
2021-03-13 11:22:16 -0500

    refined counter, review function/class components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
 import Counter from "./state/Counter";
+import Split from "./composition/Split";
+import Tooltip from "./composition/Tooltip";
 
 export default class App extends Component {
   render() {
-    return <Counter />;
+    // make 2 tooltips here and another inside the App directly
+    const firstTooltip = (
+      <Tooltip color="hotpink" message="tooltip message">
+        ipsum
+      </Tooltip>
+    );
+    const secondTooltip = (
+      <Tooltip color="#126BCC" message="another tooltip message">
+        officiis
+      </Tooltip>
+    );
+    return (
+      <main>
+        <Split className="left" flexBasis={2}>
+          This is the content for the left `Split`. Lorem {firstTooltip} ipsum
+          dolor sit amet consectetur, adipisicing elit. Incidunt ex velit
+          suscipit facere officia?
+          <Tooltip message="one more tooltip message">Necessitatibus?</Tooltip>
+        </Split>
+        <Split className="right">
+          This is the content for the left `Split`. Lorem {secondTooltip} ipsum
+          dolor sit amet consectetur, adipisicing elit. Incidunt ex velit
+          suscipit facere officia?
+        </Split>
+      </main>
+    );
   }
 }
--- src/state/Counter.js
+++ src/state/Counter.js
@@ -3,20 +3,22 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    countUp: this.props.step,
+    countUp: 1,
   };
 
   handleButtonClick = () => {
-    const newCount = this.state.count + 1;
+    const newCount = this.state.count + this.state.countUp;
     this.setState({
       count: newCount,
     });
   };
 
   render() {
+    const { count } = this.state;
+
     return (
       <div>
-        <p>The current count: {this.state.count}</p>
+        <p>The current count: {count}</p>
         <button onClick={this.handleButtonClick}>Add 1</button>
       </div>
     );
